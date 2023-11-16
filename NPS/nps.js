bb7a954a1c122417144d13fab175799b1ed581b1
Timothy Do <timothydobsa@gmail.com>
2023-11-16 02:07:49 -0800

    Well ENough.

--- NPS/nps.js
+++ NPS/nps.js
@@ -7,23 +7,28 @@ console.log('Loading NPS Blogs with nps.js...')
 function showRecent() {
 	console.log('Showing Recent Adventures!!!')
 	let recent = document.getElementById('recent');
+	recent.innerHTML = '';
 	let recentCap = 3;
 	let parksRendered = 0;
-	//let timeOut = setTimeout(function(){},100);
 	for (let park in parks) {
+		//renderPostcard(park);
+		setTimeout(function(){renderPostcard(park)},250*parksRendered);
 		parksRendered++;
-		console.log(`Rendered Postcard for ${parks[park]["name"]} (${parks[park]["alpha"]})`);
-		recent.innerHTML += `<a href='./postcards/${park}.jpg' onmouseover=\'showVisited("${park}")\'><img class='postcard' src='./postcards/${park}.jpg'></a>`;
-		// clearTimeout(timeOut);
-		// timeOut = setTimeout(function(){},100);
 	}
 }
 
+function renderPostcard(park) {
+	let recent = document.getElementById('recent');
+	console.log(`Rendered Postcard for ${parks[park]["name"]} (${parks[park]["alpha"]})`);
+	recent.innerHTML += `<a href='./postcards/${park}.jpg' onmouseover=\'showVisited("${park}")\'><img class='postcard' src='./postcards/${park}.jpg'></a>`;
+}
+
+
 function showVisited(park) {
 	let visited = document.getElementById('visited');
-	let visitedDate = new Date(parks[park]["visited"]);
+	let visitedDate = new Date(`${parks[park]["visited"]}T00:00`);
 	let weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-	let visitedString = visitedDate.toLocaleDateString('en-US');
+	let visitedString = visitedDate.toLocaleDateString();
 	visited.innerHTML = `<h2>Visited: <a href=\"https://nps.gov/${parks[park]["alpha"]}\">${parks[park]["name"]} ${parks[park]["type"]}</a> (${parks[park]["alpha"]}) on ${weekday[visitedDate.getDay()]}, ${visitedString}!`
 }
 
